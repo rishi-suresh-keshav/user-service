71ceb1d3f53b8f39ae81a9a06a51c0da8a05d9c6
Rishi Suresh Keshav <rishi.keshav@DAL-MB102.dynatacorp.com>
2020-03-22 14:21:54 -0500

    WIP

---
+++ src/model/database.go
@@ -0,0 +1,24 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func InitDatabaseConnect() {
+	// Please define your user name and password for my sql.
+	d, err := gorm.Open("mysql", "root@/user")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
+
